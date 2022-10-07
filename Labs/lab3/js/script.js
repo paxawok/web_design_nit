377e3bd11a875a035c9dbfbcb738c5f98e06ff81
paxawok <91912688+paxawok@users.noreply.github.com>
2022-10-07 12:05:28 +0300

    lab3

--- Labs/lab3/js/script.js
+++ Labs/lab3/js/script.js
@@ -14,3 +14,22 @@ if (close) {
         nav.classList.remove('active')
     })
 }
+
+$(document).ready(function(){
+    update_amounts();
+    $('.form_control, .price_control').on('keyup keypress blur change', function(e){
+        update_amounts();
+    });
+});
+
+function update_amounts(){
+    var sum = 0.0;
+    $('#table_c > tbody > tr').each(function(){
+        var qty = $(this).find('.form_control').val();
+        var price = $(this).find('.price_control').val();
+        var amount = (qty*price);
+        sum += amount;
+        $(this).find('.amount').text('' + amount);
+    });
+    $('.total').text(sum);
+}
